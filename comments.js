c58fb078d15df1fbee42b1d31d8b54208c5fa50f
Noel Brown <88703791+hesjayden@users.noreply.github.com>
2024-09-01 14:06:38 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,14 @@
+// Create web server with express
+// const express = require('express');
+// const app = express();
+// const port = 3000;
+
+// app.get('/comments', (req, res) => {
+//   res.send('This is a page with comments');
+// });
+
+// app.listen(port, () => {
+//   console.log(`Server is listening at http://localhost:${port}`);
+// });
+// Run the server and open the browser at http://localhost:3000/comments
+// You should see the message This is a page with comments
